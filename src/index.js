9839a3c9acea9187154d19930f971f820add0152
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2023-12-28 00:04:48 +0530

    Updated to use data.js, menucontroller.js, subjectPageController.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import {MenuController}
-    from "./menuController";
+import { MenuController } from "./menuController";
 import StarterController from "./starterController";
+import SubjectPageController from "./subjectPageController";
+import "./styles/style.css";
+import Data from "./Data/data";
 
 MenuController();
 StarterController();
+SubjectPageController();
+Data();
