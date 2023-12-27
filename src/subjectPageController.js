b8649fa3a58450f00612aaaba275b182ac4aee17
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2023-12-28 00:02:41 +0530

    Add subjectPageController.js

---
+++ src/subjectPageController.js
@@ -0,0 +1,70 @@
+import SubjectData from "./Data/subjectData";
+import SubjectTile from "./components/subjectTile";
+
+const subjectAdder = document.querySelector(".sub-pg-wrapper .add");
+const subjectList = document.querySelector(".sub-pg-wrapper .subject-list");
+const message = subjectList.querySelector("p");
+const input = subjectAdder.querySelector("input");
+
+function addSubject() {
+  const subjectName = input.value;
+
+  // Check if subject name is given or not
+  if (subjectName == "") {
+    message.style.color = "red";
+    message.innerText = "Please enter a subject name";
+  } else {
+    // Add subject to local storage
+    SubjectData.addSubjects(subjectName);
+
+    // Clear input and focus on it
+    input.value = "";
+    input.focus();
+
+    // Render subjects from local storage
+    renderSubjects();
+  }
+}
+
+function renderSubjects() {
+  // Get subjects array from local storage
+  const subjects = SubjectData.getSubjects();
+
+  // Check if subjects array is not empty
+  if (subjects.length != 0) {
+    // Clear the message
+    subjectList.innerHTML = "";
+
+    // Render each subject
+    subjects.forEach((subject) => {
+      subjectList.appendChild(SubjectTile(subject.name, subject.id));
+    });
+
+    // Add listeners to remove subjects and update local stoage accordingly
+    removeSubjectController();
+  }
+}
+
+function removeSubjectController() {
+  const cutIcons = subjectList.querySelectorAll(".tile > img");
+
+  cutIcons?.forEach((icon) => {
+    icon.addEventListener("click", () => {
+      const id = icon.parentElement.getAttribute("data-id");
+      SubjectData.removeSubject(id);
+    });
+  });
+}
+
+function SubjectPageController() {
+  const btn = subjectAdder.querySelector("button");
+
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    addSubject();
+  });
+
+  renderSubjects();
+}
+
+export default SubjectPageController;
