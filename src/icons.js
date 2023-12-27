2d1d9b8e4ebc2bd9e78e73e6ffecd237c4789319
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2023-12-27 16:55:20 +0530

    Add icons.js

---
+++ src/icons.js
@@ -0,0 +1,11 @@
+import home from "./icons/home.svg";
+import subjects from "./icons/subjects.svg";
+import schedule from "./icons/schedule.svg";
+import extraclass from "./icons/extraclass.svg";
+
+function Icons() {
+    const icons = [ home, subjects, schedule, extraclass ];
+    return icons;
+}
+
+export default Icons();
