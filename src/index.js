9f49fe895b8c5145577d00f8f10eb0aa8679dc01
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2023-12-28 20:22:40 +0530

    Add call to SchedulePageController()

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import StarterController from "./starterController";
 import SubjectPageController from "./subjectPageController";
 import "./styles/style.css";
 import Data from "./Data/data";
+import SchedulePageController from "./schedulePageController";
 
 Data();
 StarterController();
 MenuController();
 SubjectPageController();
+SchedulePageController();
