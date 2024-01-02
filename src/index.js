83654974a1573dc263f9ce2184229c647a484059
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-01-02 22:14:49 +0530

    Removes call to page controllers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import { MenuController } from "./menuController";
 import StarterController from "./starterController";
-import SubjectPageController from "./subjectPageController";
 import "./styles/style.css";
 import Data from "./Data/data";
-import SchedulePageController from "./schedulePageController";
-import HomePageController from "./homePageController";
 
 Data();
 StarterController();
 MenuController();
-SubjectPageController();
-SchedulePageController();
-HomePageController();
