f99db0ae79a832a432fb476104d6d1e9c2199354
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-01-02 22:17:06 +0530

    Add call to respective pagecontroller when page is rendered

--- src/mainController.js
+++ src/mainController.js
@@ -1,3 +1,7 @@
+import HomePageController from "./homePageController";
+import SchedulePageController from "./schedulePageController";
+import SubjectPageController from "./subjectPageController";
+
 class MainController {
     static render(tabID) {
         MainController.clearScreen();
@@ -5,6 +9,17 @@ class MainController {
         const pageID = `${tabID}-page`;
         const page = document.querySelector(`#${pageID}`);
         page.style.display = 'flex';
+
+        switch (pageID) {
+            case 'home-page': HomePageController();
+                break;
+            case 'subjects-page':
+                SubjectPageController();
+                break;
+            case 'schedule-page':
+                SchedulePageController();
+                break;
+        }
     }
     static clearScreen() {
         const pages = document.querySelectorAll('#main>.page');
