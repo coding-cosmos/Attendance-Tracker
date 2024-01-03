abe66d82deec4d7a90c9f4f1849d3875c45615eb
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-01-03 19:13:07 +0530

    Add todayData.js

---
+++ src/Data/todayData.js
@@ -0,0 +1,48 @@
+import { getDate,getDay } from "./date";
+import ScheduleData from "./scheduleData";
+
+class TodayData{
+    static attendance = [];
+    static setLocalStorage() {
+        if (JSON.parse(localStorage.getItem("today")) == getDate()) {
+            console.log("today..");
+           TodayData.todayReportData();
+        } else {
+          console.log("today data");
+          localStorage.setItem("today", JSON.stringify(getDate()));
+            TodayData.todayReportData();
+            
+        }
+    }
+
+    static addAttendance(subject, status) {
+        TodayData.attendance.find(attendance => attendance.subject.name == subject.name).status = status;
+        TodayData.updateLocalStorage();
+    }
+
+    static todayReportData() { 
+        console.log(JSON.parse(localStorage.getItem('todayReport'))== " ");
+        if (JSON.parse(localStorage.getItem("todayReport")) == "") {
+          const subjects = ScheduleData.getDaySchedule(getDay());
+          console.log(subjects);
+          subjects.forEach((subject) => {
+            TodayData.attendance.push({ subject, status: "Select" });
+          });
+          TodayData.updateLocalStorage();
+        } else {
+          TodayData.attendance = JSON.parse(
+            localStorage.getItem("todayReport")
+          );
+          TodayData.updateLocalStorage();
+        }
+    }
+
+    static updateLocalStorage() {
+        localStorage.setItem(
+          "todayReport",
+          JSON.stringify(TodayData.attendance)
+        );
+    }
+}
+
+export default TodayData;
