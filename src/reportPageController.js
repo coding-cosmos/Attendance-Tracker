c6313d48b7ac1200fe67aa444217480345a3fb4e
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-01-03 23:49:33 +0530

    Add ReportPageController() function

---
+++ src/reportPageController.js
@@ -0,0 +1,19 @@
+import ReportData from "./Data/reportData";
+import row from "./components/row";
+
+function ReportPageController() {
+    const homePage = document.querySelector('#home-page');
+    const reportContent = homePage.querySelector('.table-content');
+    ReportData.setReport();
+    renderSubjects(reportContent);
+    console.log(reportContent);
+} 
+function renderSubjects(reportContent) {
+    reportContent.innerHTML = '';
+   
+    ReportData.getReport().forEach(data => {
+        reportContent.appendChild(row(data.subject.id, data.subject.name, data.total, data.attended, (data.attended / data.total) * 100));
+    });
+}
+
+export default ReportPageController;
