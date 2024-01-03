9874bda19f60f300d1a579ac4acc76df4d652c12
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-01-03 19:11:24 +0530

    Add calls to page controllers

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import { MenuController } from "./menuController";
 import StarterController from "./starterController";
 import "./styles/style.css";
 import Data from "./Data/data";
+import HomePageController from "./homePageController";
+import {SchedulePageController} from "./schedulePageController";
+import SubjectPageController from "./subjectPageController";
 
 Data();
 StarterController();
 MenuController();
+SubjectPageController();
+SchedulePageController();
+HomePageController();
