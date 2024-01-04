6534e1dbc2068e80dd84cbcfbeace1b29fc2f56b
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-01-04 20:03:09 +0530

    Minify CSS

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -17,4 +18,9 @@ module.exports = merge(common, {
       filename: "[name].[contenthash].css",
     }),
   ],
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ],
+  },
 });
