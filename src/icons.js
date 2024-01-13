0d35b090f32393fabedc4afc417d478d33d51d3b
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-01-13 16:16:23 +0530

    Add settings tab in menu

--- src/icons.js
+++ src/icons.js
@@ -2,9 +2,10 @@ import home from "./icons/home.svg";
 import subjects from "./icons/subjects.svg";
 import schedule from "./icons/schedule.svg";
 import extraclass from "./icons/extraclass.svg";
+import settings from "./icons/settings.svg"
 
 function Icons() {
-    const icons = [ home, subjects, schedule, extraclass ];
+    const icons = [ home, subjects, schedule, extraclass, settings ];
     return icons;
 }
 
